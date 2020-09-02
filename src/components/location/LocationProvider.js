8754d0768600933aa67e75fd0beb65dbe36edd78
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-09-02 15:31:04 -0500

    added comments

--- src/components/location/LocationProvider.js
+++ src/components/location/LocationProvider.js
@@ -2,21 +2,22 @@ import React, { useState, useEffect } from "react"
 
 
  //The context is imported and used by individual components that need data
-
 export const LocationContext = React.createContext()
 
-/*
- This component establishes what data can be used.
- */
+
+//This component establishes what data can be used.
+//Instead of many functions, one big function is used
 export const LocationProvider = (props) => {
     const [locations, setLocations] = useState([])
 
+    //pretty familiar
     const getLocations = () => {
         return fetch("http://localhost:8088/locations")
             .then(res => res.json())
             .then(setLocations)
     }
 
+    //C of CRUD
     const addLocation = location => {
         return fetch("http://localhost:8088/locations", {
             method: "POST",
@@ -34,6 +35,8 @@ export const LocationProvider = (props) => {
         and the `getLocation` function as keys. This
         allows any child elements to access them.
     */
+
+    //THE return statement of the function
     return (
         <LocationContext.Provider value={{
             locations, addLocation, getLocations
