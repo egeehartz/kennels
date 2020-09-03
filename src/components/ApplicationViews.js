e7f62ce586562915391d11638f770681e2d7efc9
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-09-03 14:31:18 -0500

    added Employee route to AppViews

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,9 +3,11 @@ import { Route } from "react-router-dom"
 import { LocationProvider } from "./location/LocationProvider"
 import { AnimalProvider } from "./animal/AnimalProvider"
 import {CustomerProvider} from "./customer/CustomerProvider"
+import {EmployeeProvider} from "./employee/EmployeeProvider"
 import { LocationList } from "./location/LocationList"
 import { AnimalList } from "./animal/AnimalList"
 import {CustomerList} from "./customer/CustomerList"
+import {EmployeeList} from "./employee/EmployeeList"
 
 
 export const ApplicationViews = (props) => {
@@ -29,6 +31,11 @@ export const ApplicationViews = (props) => {
                     <CustomerList />
                 </Route>
             </CustomerProvider>
+            <EmployeeProvider>
+                <Route path="/employees">
+                    <EmployeeList />
+                </Route>
+            </EmployeeProvider>
         </>
     )
 }
