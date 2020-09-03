e0b497b358cf09eb1c04c6f827e32cfb256a602f
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-09-03 09:21:54 -0500

    filled out the location component to render locations

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,22 +1,16 @@
 import React from "react"
-import { Animal } from "./animal/Animal.js"
-import "./animal/Animal.css"
+import { LocationList } from "./location/LocationList"
+import { LocationProvider } from "./location/LocationProvider"
+import "./Kennel.css"
 
 export const Kennel = () => (
     <>
         <h2>Nashville Kennels</h2>
         <small>Loving care when you're not there.</small>
 
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
-        <h2>Animals</h2>
-        <article className="animals">
-            <Animal />
-            <Animal />
-            <Animal />
-        </article>
+        <h2>Locations</h2>
+        <LocationProvider>
+            <LocationList />
+        </LocationProvider>
     </>
 )
