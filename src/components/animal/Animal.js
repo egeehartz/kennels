3785143727883f30cb7aaf6f0ae4d038a8aadd02
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-09-04 10:53:13 -0500

    added animal details to HTML representation

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -5,5 +5,7 @@ export default ({animal}) => (
      <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
         <div className="animal__breed">Breed: {animal.breed}</div>
+        <div className="animal__customer">Owner: {animal.customerId}</div>
+        <div className="animal__location">Kennel: {animal.locationId}</div>
     </section> 
     )
